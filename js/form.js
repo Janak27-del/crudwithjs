96438b6bdac9494e74c26d6f9b5f24c46b7ad477
Janak27 <janakkandel27@gmail.com>
2026-02-10 21:49:47 +0545

    updated form.js fully

--- js/form.js
+++ js/form.js
@@ -1,36 +1,79 @@
 import { addItem, updateItemName } from "./app.js";
 
 export function createForm(editId = null, itemToEdit = null) {
-    const form = document.createElement("form");
-    form.className = "grocery-form";
+  const form = document.createElement("form");
+  form.className = "grocery-form";
 
-    form.innerHTML = `
+  form.innerHTML = `
     <h3>grocery bud</h3>
     <div class="form-control">
       <input type="text" class="form-input" placeholder="e.g. eggs" value="${itemToEdit ? itemToEdit.name : ""
-        }" />
+    }" />
       <button type="submit" class="submit-btn">
         ${editId ? "edit" : "submit"}
       </button>
     </div>
   `;
 
-    // Provide feedback element for alerts
-    const alert = document.createElement("p");
-    alert.className = "alert";
-    form.prepend(alert); // Add alert to top of form
+  // Provide feedback element for alerts
+  const alert = document.createElement("p");
+  alert.className = "alert";
+  form.prepend(alert); // Add alert to top of form
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const value = form.querySelector(".form-input").value;
-        if (!value) return; // Prevent empty submission
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = form.querySelector(".form-input").value;
+    if (!value) return; // Prevent empty submission
 
-        if (editId) {
-            updateItemName(value);
-        } else {
-            addItem(value);
-        }
-    });
+    if (editId) {
+      updateItemName(value);
+    } else {
+      addItem(value);
+    }
+  });
 
-    return form;
+  return form;
+}
+
+import { addItem, updateItemName } from "./app.js"; // edited
+
+// Create Form Element
+export function createForm(editId, itemToEdit) {
+  const form = document.createElement("form");
+
+  // added value and dynamic button name
+  form.innerHTML = `
+    <h2>grocery bud</h2>
+    <div class="form-control">
+     <input
+        type="text"
+        class="form-input"
+        placeholder="e.g. eggs"
+        value="${itemToEdit ? itemToEdit.name : ""}"
+      />
+      <button type="submit" class="btn">
+        ${editId ? "edit item" : "add item"}
+      </button>
+    </div>
+  `;
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = form.querySelector(".form-input");
+    const value = input.value.trim();
+
+    if (!value) {
+      alert("please provide value", "error");
+      return;
+    }
+    // added conditions
+    if (editId) {
+      updateItemName(value);
+    } else {
+      addItem(value);
+    }
+
+    input.value = "";
+  });
+
+  return form;
 }
