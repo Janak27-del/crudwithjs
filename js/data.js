c0df780e7d288fe5127dde49285144bbf2f3b652
Janak27 <janakkandel27@gmail.com>
2026-02-10 20:59:09 +0545

    data.js

---
+++ js/data.js
@@ -0,0 +1,6 @@
+export const groceryItems = [
+    { id: "1", name: "eggs", completed: true },
+    { id: "2", name: "milk", completed: true },
+    { id: "3", name: "bread", completed: false },
+    { id: "4", name: "biscuits", completed: false },
+];
